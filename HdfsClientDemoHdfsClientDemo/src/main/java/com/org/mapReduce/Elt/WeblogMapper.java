f67e51f7a34e7877383b7ec6cc59057e2ee7524f
zk <1819592017@qq.com>
2022-09-04 23:04:10 +0800

    ELK清洗数据

---
+++ HdfsClientDemoHdfsClientDemo/src/main/java/com/org/mapReduce/Elt/WeblogMapper.java
@@ -0,0 +1,34 @@
+package com.org.mapReduce.Elt;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WeblogMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+
+    private Text outK = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String lines = value.toString();
+        boolean result = Parse(lines);
+        outK.set(lines);
+        if (result) {
+            context.write(outK, NullWritable.get());
+        } else {
+            return;
+        }
+    }
+
+    private boolean Parse(String lines) {
+        String[] split = lines.split(" ");
+        if (split.length >= 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
